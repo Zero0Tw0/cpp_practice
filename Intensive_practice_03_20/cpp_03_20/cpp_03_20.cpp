708eac606e381a45ba3a12dbc6127cdfe75a2d58
Zero0Tw0 <1464001349@qq.com>
2022-03-21 22:07:27 +0800

    Day1

---
+++ Intensive_practice_03_20/cpp_03_20/cpp_03_20.cpp
@@ -0,0 +1,56 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//
+//#include<iostream>
+//#include<vector>
+//#include<algorithm>
+//#include<string>
+//using namespace std;
+//
+////Day1
+////
+//int main()
+//{
+//    int n = 0;
+//    cin >> n;
+//    vector<int> num(3 * n);
+//    for (int i = 0; i < 3 * n; i++)
+//    {
+//        int tmp = 0;
+//        cin >> num[i];
+//    }
+//    sort(num.begin(), num.end());
+//    int sum = 0;
+//    for (int i = 1; i <= n; i++)
+//    {
+//        //cout<< num[3*n - 2* i] << ' ';
+//        sum += num[3 * n - 2 * i];
+//    }
+//    cout << sum;
+//    return 0;
+//}
+//
+//int main()
+//{
+//    string s1;
+//    string s2;
+//    getline(cin, s1);
+//    getline(cin, s2);
+//    for (int i = 0; i < s2.size(); i++)
+//    {
+//        for (int j = 0; j < s1.size();)
+//        {
+//            if (s2[i] == s1[j])
+//            {
+//                s1.erase(j, 1);
+//            }
+//            else
+//            {
+//                j++;
+//            }
+//        }
+//    }
+//    cout << s1;
+//    return 0;
+//}
+
+
